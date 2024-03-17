98e28bba0a0a8e3464131a854bfdbf2539d3bf6a
ftmhabt <ftmh.a78@gmail.com>
2024-03-17 17:44:30 +0330

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Search from './components/search';
 import BookHolder from './components/bookHolder';
+import { ReactReader } from 'react-reader'
 
 function App() {
   const [books,setBooks]=useState([]);
+  const [isSelected,setIsSelected]=useState(false);
 
   return (
+    isSelected?<ReactReader/>:
     <div  className='max-w-[1000px] mx-auto my-0 px-[5rem]'>
       <Search />
       <BookHolder />
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,5 +1,15 @@
 export default function Search() {
   return (
-    <div>Search</div>
+    <div className="p-[2rem] flex justify-between">
+      <div>
+        TapTale
+      </div>
+      <div>
+        <input type="search" />
+      </div>
+      <div>
+        <button>import</button>
+      </div>
+    </div>
   )
 }
